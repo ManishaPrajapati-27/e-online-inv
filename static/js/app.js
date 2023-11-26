1e210ad7af9d459b8410eb7c39ae7866a51843e0
Manisha <manprajapati268@gmail.com>
2023-11-27 01:51:18 +0530

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,39 @@
+// // AOS
+// AOS.init({
+//   duration: 700,
+//   easing: "ease",
+//   once: true,
+//   disable: "mobile",
+// });
+
+const prev = document.querySelector(
+  ".instructor-carousel .owl-nav .owl-next span"
+);
+// const next = document.querySelector(
+//   ".instructor-carousel .owl-nav .owl-next span"
+// );
+
+console.log(prev);
+
+// const buttonChange = () => {
+//   prev.innerHTML = `<i class="far fa-arrow-right"></i>`;
+//   next.innerHTML = `<i class="far fa-arrow-left"></i>`;
+// };
+
+// buttonChange();
+
+// document.getElementById("wrapper2").onclick = function () {
+//   var className = " " + wrapper2.className + " ";
+
+//   this.className = ~className.indexOf(" active ")
+//     ? className.replace(" active ", " ")
+//     : this.className + " active";
+// };
+
+// document.getElementById("wrapper3").onclick = function () {
+//   var className = " " + wrapper3.className + " ";
+
+//   this.className = ~className.indexOf(" active ")
+//     ? className.replace(" active ", " ")
+//     : this.className + " active";
+// };
