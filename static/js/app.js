51ecbe693feb64ff8a2569a03f5026e2dce2c736
Manisha <manprajapati268@gmail.com>
2023-11-27 10:00:20 +0530

    some fixes

--- static/js/app.js
+++ static/js/app.js
@@ -6,34 +6,19 @@
 //   disable: "mobile",
 // });
 
-const prev = document.querySelector(
-  ".instructor-carousel .owl-nav .owl-next span"
-);
-// const next = document.querySelector(
+// const prev = document.querySelector(
 //   ".instructor-carousel .owl-nav .owl-next span"
 // );
+// console.log(prev);
 
-console.log(prev);
-
-// const buttonChange = () => {
-//   prev.innerHTML = `<i class="far fa-arrow-right"></i>`;
-//   next.innerHTML = `<i class="far fa-arrow-left"></i>`;
-// };
-
-// buttonChange();
-
-// document.getElementById("wrapper2").onclick = function () {
-//   var className = " " + wrapper2.className + " ";
-
-//   this.className = ~className.indexOf(" active ")
-//     ? className.replace(" active ", " ")
-//     : this.className + " active";
-// };
-
-// document.getElementById("wrapper3").onclick = function () {
-//   var className = " " + wrapper3.className + " ";
-
-//   this.className = ~className.indexOf(" active ")
-//     ? className.replace(" active ", " ")
-//     : this.className + " active";
-// };
+document.addEventListener("DOMContentLoaded", function () {
+  // Your code here
+  const prev = document.querySelector(
+    ".instructor-carousel .owl-nav .owl-prev span"
+  );
+  const next = document.querySelector(
+    ".instructor-carousel .owl-nav .owl-next span"
+  );
+  prev.innerHTML = `<i class="far fa-long-arrow-left"></i>`;
+  next.innerHTML = `<i class="far fa-long-arrow-right"></i>`;
+});
